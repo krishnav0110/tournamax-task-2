0d17af69e7a3ef6d2e59f50fcaed2b99fb2f8d50
krishnav0110 <krishnavishwakarma@student.sfit.ac.in>
2024-12-29 15:04:50 +0530

    idk

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -30,7 +30,7 @@ export default function MyMap(props: any) {
     { position: [19.076, 72.8777], label: "Mumbai" },
     { position: [13.0827, 80.2707], label: "Chennai" },
     { position: [22.5726, 88.3639], label: "Kolkata" },
-    { position: [12.9716, 77.5946], label: "Bangalore" },
+    // { position: [12.9716, 77.5946], label: "Bangalore" },
   ];
 
 
